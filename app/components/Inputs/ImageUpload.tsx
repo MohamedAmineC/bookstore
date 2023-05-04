c61c6d0650dc0f128d189d31cad819911166b89d
Mohmaed Amine Chalouati <mohamedaminechalouati@gmail.com>
2023-05-04 10:50:22 +0200

    adding removing image functionnlaity to the upload

--- app/components/Inputs/ImageUpload.tsx
+++ app/components/Inputs/ImageUpload.tsx
@@ -3,6 +3,7 @@
 import {CldUploadWidget} from "next-cloudinary"
 import Image from "next/image"
 import { useCallback } from "react"
+import { IoMdClose } from "react-icons/io"
 import {TbPhotoPlus} from "react-icons/tb"
 
 declare global {
@@ -25,6 +26,9 @@ const ImageUpload:React.FC<ImageUploadProps> = ({
       }
     }
   }, [onChange, value]);
+  const handleRemoveImage = useCallback((image:string) => {
+    onChange(value.filter(item => item !== image))
+  },[onChange,value])
   console.log(value)
   return (
     <CldUploadWidget 
@@ -48,7 +52,7 @@ const ImageUpload:React.FC<ImageUploadProps> = ({
                 className="absolute inset-0 grid w-full h-full grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 overflow-auto"
                 >
                   {value?.map((image) => (
-                    <div key={image} className="col-span-1">
+                    <div key={image} className="relative col-span-1">
                       <Image 
                       alt="Upload Image"
                       width={500}
@@ -57,6 +61,10 @@ const ImageUpload:React.FC<ImageUploadProps> = ({
                       src={image}
                       className="col-span-1 h-full"
                       />
+                      <div className="absolute top-2 right-1 bg-red-600/30 z-10 w-10 h-10 rounded-full text-white grid place-items-center"
+                      onClick={() => handleRemoveImage(image)}>
+                        <IoMdClose />
+                      </div>
                     </div>
                     ))}
                 </div>
